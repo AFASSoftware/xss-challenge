6ffe3a120fc9f03352ff81baf49c0e7e2895271b
Johan Gorter <johan-gorter@users.noreply.github.com>
2025-06-17 08:25:21 +0000

    port matches container

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
-
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+
+// Configure to use port 5000
+builder.Configuration["urls"] = "http://localhost:5000";
 
 var app = builder.Build();
 
